9737ff33c3665a970e96a4df0979d094a25e0c55
Anton Petrochenko <petrochenko.ai@ubmail.ru>
2022-10-02 18:12:12 +0600

    bigfont

--- textmsg.lua
+++ textmsg.lua
@@ -1,11 +1,19 @@
-return function ()
+return function (msg)
   world:add({
+    y = 99999,
     timer = 400,
     update = function (self, dt)
-      
+      self.timer = self.timer - dt*200
     end,
     draw = function (self, dt)
-
+      love.graphics.setColor(0,0,0,1)
+      for xi=-1,1 do
+          for yi=-1,1 do
+              love.graphics.print(msg,math.floor(self.timer)+xi,160+yi)
+          end
+      end
+      love.graphics.setColor(1,1,1,1)
+      love.graphics.print(msg,math.floor(self.timer),160)
     end
   })
 end
